7f12ee8a352cbd0c9c39632739e476992f265a70
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-08 00:42:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet >= 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do."
+  }
 }
 
 function ternaryCheckCity(){
